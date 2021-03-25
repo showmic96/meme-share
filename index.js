9f6939428efb225b189730aa0f16daf8e790f0f2
Showmic <showmic96@gmail.com>
2021-03-25 23:24:44 +0600

    Add insomnia exports

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const config = require('config');
 const bodyParser = require('body-parser')
-const multer = require('multer');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const upload = multer();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
